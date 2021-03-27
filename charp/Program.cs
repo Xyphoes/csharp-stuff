c4cbad6499b7445ec126cdbde76f79dbb84605fb
Xyphoes <49097995+Xyphoes@users.noreply.github.com>
2021-03-27 18:49:35 +0530

    Add files via upload

---
+++ charp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace charp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello. The name of your hero is...");
+            Console.ReadLine();
+
+            Console.WriteLine("Walked into a bar and ordered a...");
+            Console.ReadLine();
+
+            Console.WriteLine("He sat there drinking his drink when a dragon spewed fire onto him and");
+            Console.WriteLine("your hero was killed by a Dragon.\n The End");
+
+            Console.ReadKey();
+        }
+    }
+}
